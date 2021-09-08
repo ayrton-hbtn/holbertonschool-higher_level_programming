1e0228963b5b68138c47bee17313057f0572f484
hippocampus3282 <3282@holbertonschool.com>
2021-09-07 17:20:11 -0700

    Project 0x01 Python - if/else, loops functions

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
 	for i in range(1, 101):
-		if i % 15 == 0:
-			print("FizzBuzz ")
-		elif i % 3 == 0:
-			print("Fizz ", end="")
-		elif i % 5 == 0:
-			print("Buzz ", end="")
-		else:
-			print("{} ".format(i), end="")
+		p = ""
+		if i % 3 == 0:
+			p += "Fizz"
+		if i % 5 == 0:
+			p += "Buzz"
+		if p == "":
+			p += str(i)
+		print("{} ".format(p), end="")
