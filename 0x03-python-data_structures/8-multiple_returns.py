6a24c723cbec5fa7ebaed00deee7a2c1d556b81e
hippocampus3282 <3282@holbertonschool.com>
2021-09-13 12:32:09 -0700

    Project 0x03 Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return None
-    a = len(sentence), sentence[0]
-    return a
+    l = len(sentence)
+    return l, sentence[0] if l > 0 else None
