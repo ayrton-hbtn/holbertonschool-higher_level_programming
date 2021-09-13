a8c79e18257b4a0ded030348da6b9dce43d78fe2
hippocampus3282 <3282@holbertonschool.com>
2021-09-13 12:02:04 -0700

    Project 0x03 Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int is_palindrome(listint_t **head)
+{
+    listint_t *tmp = NULL;
+    int i = 0, j = 0, arr[100];
+
+    if (!*head)
+        return (1);
+    tmp = *head;
+    while (tmp)
+    {
+        arr[i] = tmp->n;
+        tmp = tmp->next;
+        i++;
+    }
+    i -= 1;
+    for (; j <= i; j++, i--)
+    {
+        if (arr[j] != arr[i])
+            return (0);
+        else
+            continue;
+    }
+    return (1);
+}
