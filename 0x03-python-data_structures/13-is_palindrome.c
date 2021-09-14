389d1174507e1afd0787e3df877eec98593c4e5a
hippocampus3282 <3282@holbertonschool.com>
2021-09-14 14:04:52 -0700

    aber

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,40 +2,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * isPalindromeUtil - asdasd
+ * @left: asdasd
+ * @right: asdasd
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int isPalindromeUtil(listint_t **left, listint_t *right)
 {
-    int isp, isp1;
+	int isp, isp1;
 
-    /* stop recursion when right becomes NULL */
-    if (right == NULL)
-        return (1);
- 
-    /* If sub-list is not palindrome then no need to
-    check for current left and right, return false */
-    if (isPalindromeUtil(left, right->next))
-        isp = 1;
-    else
-        isp = 0;
-    if (!isp)
-        return (0);
- 
-    /* Check values at current left and right */
-    if (right->n == (*left)->n)
-        isp1 = 1;
-    else
-        isp1 = 0;
- 
-    /* Move left to next node */
-    *left = (*left)->next;
- 
-    return (isp1);
+	if (right == NULL)
+		return (1);
+
+	if (isPalindromeUtil(left, right->next))
+		isp = 1;
+	else
+		isp = 0;
+	if (!isp)
+		return (0);
+
+	if (right->n == (*left)->n)
+		isp1 = 1;
+	else
+		isp1 = 0;
+
+	*left = (*left)->next;
+
+	return (isp1);
 }
- 
-// A wrapper over isPalindromeUtil()
+
+/**
+ * is_palindrome - asdasd
+ * @head: asdasd
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
-    int check;
+	int check;
 
-    check = isPalindromeUtil(head, *head);
-    return (check);
+	check = isPalindromeUtil(head, *head);
+	return (check);
 }
