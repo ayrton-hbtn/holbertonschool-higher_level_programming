901bb4223a614498a15e0f29411f6cf64b94486d
hippocampus3282 <3282@holbertonschool.com>
2021-09-15 16:36:12 -0700

    Project 0x04 Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,13 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) is not str:
         return 0
     roman_values = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500, 
-            'M':1000
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
             }
     res = 0
     for letter in roman_string:
