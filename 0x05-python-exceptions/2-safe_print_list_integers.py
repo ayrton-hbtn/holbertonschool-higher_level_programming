df4f941257553939048bc95435d72b4ef2ebdb58
hippocampus3282 <3282@holbertonschool.com>
2021-09-27 14:00:19 -0700

    project 0x05 Python - Exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    for i in my_list:
+        if x == 0:
+            break
+        try:
+            print("{:d}".format(i), end="")
+            a += 1
+        except:
+            pass
+        x -= 1
+    print("")
+    return a
