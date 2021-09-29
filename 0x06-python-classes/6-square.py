5a2f47721d21bc639d55775f6fea3813bc089c8b
hippocampus3282 <3282@holbertonschool.com>
2021-09-28 17:53:39 -0700

    Project 0x06 Python - Classes and Objects

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+''' Square class '''
+
+
+class Square:
+    ''' create Square instance with public attributes
+    size and position '''
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    ''' retrieve size and make it private'''
+    @property
+    def size(self):
+        return self.__size
+
+    ''' set private size attribute '''
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    ''' retrieve position and make it private '''
+    @property
+    def position(self):
+        return self.__position
+
+    ''' set private position attribute '''
+    @position.setter
+    def position(self, value):
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position[0] = value[0]
+        self.__position[1] = value[1]
+
+    ''' calculates area of square '''
+    def area(self):
+        return self.__size ** 2
+
+    ''' print a square of # the size of self.__size'''
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for line in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for space in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
