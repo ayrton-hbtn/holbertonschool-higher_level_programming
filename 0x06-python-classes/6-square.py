aa7628bad956c80806df1bb78aaf50ae213ca0d9
hippocampus3282 <3282@holbertonschool.com>
2021-09-28 17:57:23 -0700

    Project 0x06 Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,8 +33,7 @@ class Square:
     def position(self, value):
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position[0] = value[0]
-        self.__position[1] = value[1]
+        self.__position = value
 
     ''' calculates area of square '''
     def area(self):
