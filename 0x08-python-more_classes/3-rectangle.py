897910f004fc9b1bfba9b70f9bb591b51f4aea23
hippocampus3282 <3282@holbertonschool.com>
2021-10-04 08:51:04 -0700

    Project 0x08 Python - More Classes and Objects

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+'''Define Rectangle object
+'''
+
+
+class Rectangle:
+    '''Constructor method with width and height
+    '''
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    ''' width getter '''
+    @property
+    def width(self):
+        return self.__width
+
+    ''' height getter '''
+    @property
+    def height(self):
+        return self.__height
+
+    ''' width setter '''
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    ''' height setter '''
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    ''' calculates area of rectangle object '''
+    def area(self):
+        return self.__width * self.__height
+
+    ''' calculates perimeter of rectangle object '''
+    def perimeter(self):
+        return (self.__width * 2) + (self.__height * 2)
+
+    ''' make object readable and printable,
+        generates output for user
+    '''
+    def __str__(self):
+        p = ''
+        for line in range(self.__height):
+            for col in range(self.__width):
+                p += '#'
+            p += '\n'
+        return p[:-1]
