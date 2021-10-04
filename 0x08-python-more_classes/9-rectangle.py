11cc1c7a7123dd398e701531747a7951af2ba790
hippocampus3282 <3282@holbertonschool.com>
2021-10-04 14:36:14 -0700

    Project 0x08 Python - More Classes and Objects

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -45,9 +45,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     ''' calculates area of rectangle object '''
