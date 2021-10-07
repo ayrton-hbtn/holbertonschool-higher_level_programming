d5a065e595c8ae8a970961d59a38c4096e3d8ffe
hippocampus3282 <3282@holbertonschool.com>
2021-10-06 17:36:16 -0700

    Project 0x07 Python - Test-driven development

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    multiplies two matrices with numpy
+'''
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    '''
+        multiplies m_a with m_b
+    '''
+    return np.dot(m_a, m_b)
