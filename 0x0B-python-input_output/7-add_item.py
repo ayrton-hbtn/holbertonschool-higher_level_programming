1b06e7131647f4ddf98d6cf9a9358172860244fe
hippocampus3282 <3282@holbertonschool.com>
2021-10-13 14:26:00 -0700

    Project 0x0B Python - File I/O

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,13 @@ to JSON file
 
 
 from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_l = []
 try:
     my_l = load_from_json_file("add_item.json") + argv[1:]
     save_to_json_file(l, "add_item.json")
 except Exception:
-    save_to_json_file(l, "add_item.json")
+    save_to_json_file(argv[1:], "add_item.json")
