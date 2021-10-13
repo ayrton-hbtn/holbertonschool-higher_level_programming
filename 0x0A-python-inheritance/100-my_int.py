1c6546bc01e49b0c1a01536e5bc2a2d4440d50d6
hippocampus3282 <3282@holbertonschool.com>
2021-10-12 17:28:25 -0700

    rebel int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+MyInt class inherits from int
+'''
+
+
+class MyInt(int):
+    '''
+    Works the same of int, but operators
+    == and != are inverted
+    '''
+    def __eq__(self, other):
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        return not super().__eq__(other)
