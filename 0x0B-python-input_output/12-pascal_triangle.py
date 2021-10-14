8c162da265c1421ff927f34b53b218c80b272d0d
hippocampus3282 <3282@holbertonschool.com>
2021-10-13 19:09:43 -0700

    Project 0x0B Python - File I/O

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Pascal triangle
+'''
+
+
+def pascal_triangle(n):
+    '''
+    Pascal triangle
+    '''
+    triangle = [[1]]
+    prev = []
+    current = 0
+    if n <= 0:
+        return triangle
+    while n >= 2:
+        prev = triangle[current]
+        row = [1]
+        for i in range(len(prev)):
+            try:
+                row.append(prev[i] + prev[i + 1])
+            except Exception:
+                row.append(1)
+        triangle.append(row)
+        current += 1
+        n -= 1
+    return triangle
