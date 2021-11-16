6a8358d74169eb6a02121841b36f75a4d607f201
hippocampus3282 <3282@holbertonschool.com>
2021-11-16 10:52:48 -0800

    repeated score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of records with same score in second_table
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
