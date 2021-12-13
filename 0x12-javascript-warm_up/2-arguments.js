ef6bcfdef025f758ff5ce0ec712d1d3420d58b27
hippocampus3282 <3282@holbertonschool.com>
2021-12-13 09:47:46 -0800

    typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const args = process.argv.length;
-
 if (args < 3) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (args === 3) {
   console.log('Argument found');
 } else {
