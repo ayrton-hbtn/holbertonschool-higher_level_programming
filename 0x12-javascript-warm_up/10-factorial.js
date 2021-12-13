510a0847404135021e735648b7062b331350531f
hippocampus3282 <3282@holbertonschool.com>
2021-12-13 10:58:10 -0800

    fix: RangeError

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 
-const argv = process.argv;
-if (!parseInt(argv[2])) {
-  console.log(1);
-}
-
 function factorial (a) {
   if (a <= 1) {
     return 1;
+  } else {
+    return (a * factorial(a - 1));
   }
-  return (a * factorial(a - 1));
 }
 
-console.log(factorial(argv[2]));
+const argv = process.argv;
+
+if (!argv[2] || !parseInt(argv[2])) {
+  console.log(1);
+} else {
+  console.log(factorial(parseInt(argv[2])));
+}
