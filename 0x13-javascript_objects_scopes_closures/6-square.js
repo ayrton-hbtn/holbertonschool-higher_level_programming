f6ecc24249a41217ea0000033d75c0916f219684
root <3282@holbertonschool.com>
2021-12-14 08:50:46 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const Sq = require('./5-square');
+module.exports = class Square extends Sq {
   charPrint (c) {
     let p = '';
     if (!c) {
-      Rectangle.prototype.print.call(this);
-      return;
+      c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
