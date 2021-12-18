c4e5010049088cf614781da5927d4830c77ea1cc
root <3282@holbertonschool.com>
2021-12-18 11:26:47 -0800

    comment

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import Session
-from sqlalchemy import update
-from sqlalchemy import (create_engine)
+""" Change the name of the State where id = 2 to New Mexico """
+
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import Session
+    from sqlalchemy import update
+    from sqlalchemy import (create_engine)
+
     argv = sys.argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                             argv[1], argv[2], argv[3]), pool_pre_ping=True)
