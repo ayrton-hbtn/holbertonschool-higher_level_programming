2ff516fe1ae7b99b37da5a830584aa4beb4d6be9
root <3282@holbertonschool.com>
2021-12-18 11:29:48 -0800

    comment

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine
+""" contains the class definition of a City """
+
+
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.sql.schema import ForeignKey
 from model_state import Base, State
-import sys
-
-argv = sys.argv
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
 
 class City(Base):
