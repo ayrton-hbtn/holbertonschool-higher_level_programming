92759e4f1907025e193992f625cea94615fc4329
root <3282@holbertonschool.com>
2021-12-18 11:30:43 -0800

    comment

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""  prints all City objects from the database hbtn_0e_14_usa """
 
-import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, asc
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine, asc
+
     argv = sys.argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                             argv[1], argv[2], argv[3]), pool_pre_ping=True)
