8e0d65f42d80ebcb8ab96e8097c811edd2550c8e
root <3282@holbertonschool.com>
2021-12-18 11:02:08 -0800

    comment

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+""" sql injection protected """
+
 
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
     argv = sys.argv
     server = MySQLdb.Connect(host="localhost", port=3306,
                              user=argv[1], passwd=argv[2],
