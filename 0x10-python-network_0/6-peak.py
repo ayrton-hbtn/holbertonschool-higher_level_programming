13a202e74c6403d93661c31457fd32b36fba724d
ayrton-hbtn <3282@holbertonschool.com>
2022-01-09 14:41:51 +0000

    comments

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+""" function """
+
 
 def find_peak(list_of_integers):
-	""" finds a peak in a list of unsorted integers """
-	if (list_of_integers):
-		return max(list_of_integers)
-	else:
-		return None
+    """ finds a peak in a list of unsorted integers """
+    if (list_of_integers):
+        return max(list_of_integers)
