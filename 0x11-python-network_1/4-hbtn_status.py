fb995676e2d670bd9bc9de26d287f8b68e93e5a8
root <3282@holbertonschool.com>
2022-01-10 16:42:01 +0000

    fetch with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,12 +2,10 @@
 """ fetches with 'requests' package
 """
 
-
-import requests
-
+from requests import get
 
 if __name__ == "__main__":
-    url_req = requests.get('https://intranet.hbtn.io/status')
+    data = get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(url_req.text)))
-    print("\t- content: {}".format(url_req.text))
+    print('\t- type: {}'.format(type(data.text)))
+    print('\t- content: {}'.format(data.text))
