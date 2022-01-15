fcf27203bc9bfd27cc9d0b6e2991c116c4bee515
root <3282@holbertonschool.com>
2022-01-14 21:10:25 -0800

    .

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""
-takes in a URL and an email address
-sends a POST request to the passed URL with the email as a parameter
-and finally displays the body of the response
+""" Python script
 """
 
 
-from sys import argv
-import requests as r
+import sys
+import requests
+
 
 if __name__ == "__main__":
-    email = argv[2]
-    data = {'email': email}
-    res = r.post(argv[1], data=data)
-    print(res.request.text)
+    url_request = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(url_request.text)
