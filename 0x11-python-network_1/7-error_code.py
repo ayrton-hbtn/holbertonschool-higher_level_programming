c79d5b95d13c39541fff4f6fa2adf04cee43f501
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-02-01 18:07:27 -0300

    .

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,8 +5,9 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    res = r.get(url)
+    header = {""}
+    res = r.get(url, )
     if res.status_code >= 400:
-        print(f"Error code:{res.status_code}")
+        print(f"Error code:{res.json}")
     else:
         print(res.request.body)
