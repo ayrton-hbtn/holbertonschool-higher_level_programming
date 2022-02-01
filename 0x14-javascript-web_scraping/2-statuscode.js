46962cab48779066a09f0fb96de5451fc74a9735
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-02-01 20:45:38 -0300

    request module: status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const { argv } = require('process');
+
+request(argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(res.statusCode);
+});
