2dfbe498004ca85fb23bbfbd9e7984ee47bc2043
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-02-01 20:37:43 -0300

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const { argv } = require('process');
+
+fs.writeFile(argv[2], argv[3], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+});
