5d457006dbc8c0b04ea5564a5d83f18a324dda00
ayrton-coelho <ayrtoncoelhods@gmail.com>
2022-02-01 22:21:45 -0300

    star wars characters by movie id

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const { argv } = require('process');
+const url = 'https://swapi-api.hbtn.io/api/films';
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const movie = JSON.parse(body).results[argv[2] - 1];
+  const characters = movie.characters;
+  for (const char of characters) {
+    request(char, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
